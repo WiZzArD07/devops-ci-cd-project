11e64f9d27b2f27831798f57bdce738ce672fa61
WiZzArD <arujaiswal12@gmail.com>
2026-02-25 20:04:29 +0530

    Testing auto deploy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("CI/CD on Render is Working 🚀");
+  res.send("Auto Deploy Test Successful 🔥");
 });
 
 app.listen(PORT, () => {
