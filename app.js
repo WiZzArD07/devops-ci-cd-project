606c0236c791433a27cab82713a1399e9ac660a1
WiZzArD <arujaiswal12@gmail.com>
2026-02-25 19:47:59 +0530

    Updated for Render deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("CI/CD is Working 🚀");
+  res.send("CI/CD on Render is Working 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
